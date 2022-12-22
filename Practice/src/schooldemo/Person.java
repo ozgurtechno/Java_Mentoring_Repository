4a916d6aa8257196fa31dae0f4be62de29ff8311
Ozgur Techno <ozgur.technostudy@gmail.com>
2022-12-22 10:32:19 +0100

    week6

---
+++ Practice/src/schooldemo/Person.java
@@ -0,0 +1,16 @@
+package schooldemo;
+
+public class Person {
+
+     String firstName;
+    String lastname;
+
+    public Person(String firstName, String lastname) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+    }
+
+    public Person(String firstName) {
+        this.firstName = firstName;
+    }
+}
--- Week7/src/classes/Car.java
+++ Week7/src/classes/Car.java
@@ -2,8 +2,21 @@ package classes;
 
 public class Car {
 
+    // ---------------------------------- INSTANCE FIELDS -------------------------------------------------------------
     public String model;
     public int year;
+
+    // ---------------------------------- CONSTRUCTOR -------------------------------------------------------------
+
+    public Car() {
+    }
+
+    public Car(String model, int year) {  // Constructor Overloading
+        this.model = model;
+        this.year = year;
+    }
+
+    // ---------------------------------- INSTANCE METHODS (BEHAVIOR) ----------------------------------------------
 
     public void start(){
         System.out.println("Car started...");
