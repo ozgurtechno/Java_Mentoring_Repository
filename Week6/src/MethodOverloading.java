ebaecc563be2420be81a27aeac42f6aee7040b11
Ozgur Techno <ozgur.technostudy@gmail.com>
2022-12-22 10:11:21 +0100

    week6

---
+++ Week6/src/MethodOverloading.java
@@ -0,0 +1,23 @@
+public class MethodOverloading {
+
+    public double calculate(int a, double b){
+        return a*b;
+    }
+
+    private double calculate(double a, int b){
+        return a*b;
+    }
+
+    public int calculate(int a, int b){
+        return a*b;
+    }
+}
+
+/*
+Two or more methods can have the same name inside the same class if they accept different arguments.
+This feature is known as method overloading.
+Method overloading is achieved by either:
+    - changing the number of arguments.
+    - or changing the data type of arguments.
+It is not method overloading if we only change the return type of methods. There must be differences in the number of parameters.
+ */
