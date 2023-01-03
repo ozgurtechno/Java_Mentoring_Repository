90cd681e0a1d657334eee6bf98dc09c4c8e0364c
Ozgur Techno <ozgur.technostudy@gmail.com>
2023-01-03 15:52:24 +0100

    week8

--- Week8/src/AccessModifiers/package1/SamePackage.java
+++ Week8/src/AccessModifiers/package1/SamePackage.java
@@ -21,13 +21,13 @@ public class SamePackage {
 
     public static void main(String[] args) {
 
-        Human resident1 = new Human("George", 42, "Happy Moments", "GREGOR");
+        Human human2 = new Human("George", 42, "Happy Moments", "GREGOR");
         Teacher teacher = new Teacher("Ali",44, "golf","nickname");
 
-        System.out.println("public = " + resident1.name);
-        System.out.println("protected = " + resident1.memories);
-        System.out.println("default = " + resident1.nickName);
-//        System.out.println("private = " + resident1.age);
+        System.out.println("public = " + human2.name);
+        System.out.println("protected = " + human2.memories);
+        System.out.println("default = " + human2.nickName);
+//        System.out.println("private = " + human2.age);
 
         System.out.println("public = " + teacher.name);
         System.out.println("protected = " + teacher.memories);
---
+++ Week8/src/encapsulation/Encapsulation_Demo.java
@@ -0,0 +1,19 @@
+package encapsulation;
+
+public class Encapsulation_Demo {
+
+    public static void main(String[] args) {
+
+        Course english = new Course("English", 70);
+        School school = new School("ODTU");
+
+        school.addCourse(english);
+
+        Course german = new Course("German");
+        german.setMinScore(50);
+
+
+
+
+    }
+}
