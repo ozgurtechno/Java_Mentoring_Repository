9728fc1bfd942c413e541635f862d3fd6625bad7
ozgur <ozgur.technostudy@gmail.com>
2023-05-23 09:48:18 -0400

    task1

---
+++ Week1/src/Tasks/Task_1.java
@@ -0,0 +1,50 @@
+package Tasks;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        /*
+        #1. You need to print message below using only 1 System.out.print()
+        C:\\”Program Files”\\..
+         */
+//        System.out.print("C:\\\\\"Program Files\"\\\\..");
+
+
+        /*
+       #2. You need to print message below using only 1 System.out.print()
+        “To be
+            or not
+                to be!”
+            \Shakespeare\
+         */
+//        System.out.print("\"To be\n\tor not\n\t\tto be!\"\n\t\\Shakespeare\\");
+
+        /*
+        #3. You are given two numbers(amount of minutes and seconds).
+        You need to convert it all into seconds only and print it.
+        Use Scanner class to read numbers from keyboard.
+         */
+//        Scanner in = new Scanner(System.in);
+//
+//        int minutes = in.nextInt();
+//        int seconds = in.nextInt();
+//
+//        int totalSeconds = minutes * 60 + seconds;
+//        System.out.println("Total seconds: " + totalSeconds);
+
+        /*
+        #4. You are given two numbers(price of a good and dicsount in percentage).
+        You need to find the final price(after calculating the discount) and print it.
+        Use Scanner class to read numbers from keyboard.
+         */
+
+//        Scanner in = new Scanner(System.in);
+//
+//        int price = in.nextInt();
+//        int discountInPercents = in.nextInt();
+//
+//        int discountAmount = price * discountInPercents / 100;
+//        int finalPrice = price - discountAmount;
+//
+//        System.out.println("Final price is: " + finalPrice);
+    }
+}
