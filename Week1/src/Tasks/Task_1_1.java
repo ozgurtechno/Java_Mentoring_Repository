c19ac9f199bbecca46a55411986f2b4d8f80695e
ozgur <ozgur.technostudy@gmail.com>
2023-05-29 20:58:10 -0400

    week1

---
+++ Week1/src/Tasks/Task_1_1.java
@@ -0,0 +1,50 @@
+package Tasks;
+
+public class Task_1_1 {
+
+    public static void main(String[] args) {
+
+        // Swap these 2 numbers
+        int x = 6;
+        int y = 12;
+
+
+
+        System.out.println("X is ...: " + x);
+        System.out.println("Y is ...: " + y);
+
+        // Swap without declaring third variable
+        int a = 6;
+        int b = 12;
+
+
+
+        System.out.println("A is ...: " + a);
+        System.out.println("B is ...: " + b);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+        a = a - b;  // 6 - 12 = -6
+        b = a + b;  // -6 + 12 = 6
+        a = b - a;  // 6 - -6  = 12
+ */
