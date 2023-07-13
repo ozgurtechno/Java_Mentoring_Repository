902c9192934290fa6ed551731b63f1d6adb79682
Ozgur Techno <ozgur.technostudy@gmail.com>
2023-07-13 13:28:04 -0400

    week 7 project added

--- Week7/src/AccessModifiers/Modifiers_Demo.java
+++ Week7/src/AccessModifiers/Modifiers_Demo.java
@@ -10,12 +10,12 @@ public class Modifiers_Demo {
         Human human = new Human("George", 42, "Happy Moments", "GREGOR");
         Teacher teacher = new Teacher("Ali",44, "golf","nickname");
 
-        System.out.println("public = " + human.name);
+//        System.out.println("public = " + human.name);
 //        System.out.println("protected = " + human.memories);
 //        System.out.println("default = " + human.nickName);
 //        System.out.println("private = " + human.age);
-
-        System.out.println("public = " + teacher.name);
+//
+//        System.out.println("public = " + teacher.name);
 //        System.out.println("protected = " + teacher.memories);
 //        System.out.println("default = " + teacher.nickName);
 //        System.out.println("private = " + teacher.age);
